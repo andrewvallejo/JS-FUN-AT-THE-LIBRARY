72e58a9579ec82b6604bc4e2b2042f55644b62e7
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-03-27 10:10:42 -0600

    Complete shelf

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,45 @@
+function shelfBook(book, shelf) {
+  if (shelf.length < 3) {
+    shelf.unshift(book);
+  } else {
+    return shelf;
+  }
+}
+
+function unshelfBook(book, shelf) {
+  for (let i = 0; i < shelf.length; i++) {
+    if (shelf[i].title === book) {
+      shelf.splice(i, 1);
+    }
+  }
+  return shelf;
+}
+
+function listTitles(shelf) {
+  var listShelf = "";
+  for (let i = 0; i < shelf.length; i++) {
+    listShelf += shelf[i].title + ", ";
+    var spliceShelf = listShelf.slice(0, -2);
+  }
+  return spliceShelf;
+}
+
+function searchShelf(shelf, book) {
+  var bookOnShelf = true
+  for (let i = 0; i < shelf.length; i++) {
+    if(shelf[i].title === book) {
+      bookOnShelf = true
+    } else {
+      bookOnShelf = false
+    }
+  }
+  return bookOnShelf
+}
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  unshelfBook,
+  listTitles,
+  shelfBook,
+  searchShelf
 };
