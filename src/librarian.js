76fec5a0b40db4f1d42ffad9b93acd8c20a8f0a9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-03-28 13:05:25 -0600

    Reach findbook

--- src/librarian.js
+++ src/librarian.js
@@ -1,6 +1,29 @@
 class Librarian {
-  constructor() {
-    
+  constructor(name, library) {
+    this.name = name;
+    this.library = library;
+  }
+  //Greet Patron
+  greetPatron(name, time) {
+    var isMorning = true;
+    if (isMorning === time) {
+      return `Good morning, ${name}!`;
+    } else {
+      return `Hello, ${name}!`;
+    }
+  }
+  findBook(book) {
+   var shelf = this.library.shelves.fantasy
+   var libBook = shelf[0]
+  //  console.log(libBook)
+  //   // console.log(shelf)
+   for (var i = 0; i < shelf.length; i++) {
+     var LibraryBook = shelf[i]
+      // console.log(libraryBook)
+     if (libraryBook === book) {
+      return `Yes, we have ${book}`
+    } 
+    }
   }
 }
 
