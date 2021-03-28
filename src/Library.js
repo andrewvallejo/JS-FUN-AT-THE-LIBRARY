b7c40974942178ad76f65219e1c6f77bdbbe51c9
Andrew Vallejo <andrew.a.vallejo@gmail.com>
2021-03-28 13:15:55 -0600

    format

--- src/Library.js
+++ src/Library.js
@@ -11,24 +11,24 @@ function createLibrary(name) {
 }
 
 function addBook(library, book) {
-  library.shelves[book.genre].push(book)
+  library.shelves[book.genre].push(book);
 }
 
 function checkoutBook(library, book, genre) {
   if (library.shelves[genre].length === 0) {
-     return `Sorry, there are currently no copies of ${book} available at the ${library.name}`
+    return `Sorry, there are currently no copies of ${book} available at the ${library.name}`;
   }
   for (var i = 0; i < library.shelves[genre].length; i++) {
-    var bookName = library.shelves[genre][i].title
+    var bookName = library.shelves[genre][i].title;
     if (bookName === book) {
-       library.shelves[genre].splice(genre[i], 1)
-       return `You have now checked out ${book} from the ${library.name}` 
-    } 
+      library.shelves[genre].splice(genre[i], 1);
+      return `You have now checked out ${book} from the ${library.name}`;
+    }
   }
 }
 
 module.exports = {
   createLibrary,
   addBook,
-  checkoutBook
+  checkoutBook,
 };
